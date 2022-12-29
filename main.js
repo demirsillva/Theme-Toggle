ffb5a3edbec89e0848677236e58f0545cf8dace4
demirsillva <ademirfernandessilva622@gmail.com>
2022-12-29 00:38:19 -0300

    Some bugs fixed

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const checkbox = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.add("dark");
+}
 
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle("dark");
+
+    let theme = "light";
+
+    if(document.body.classList.contains("dark")) {
+      theme = "dark";
+    }
+    
+    localStorage.setItem("theme", theme);
 });
+
+/* const theme = localStorage.getItem('theme');
+
+if( theme ) {
+  setTheme(theme);
+}
+
+localStorage.setItem('theme', checkbox); */
